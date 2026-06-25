9bf26ff3166977b0520d450769e97256c3839959
kittisakdiw87 <kittisakdiw87@gmail.com>
2026-06-25 16:52:20 +0700

    box.java

---
+++ week2/box.java
@@ -0,0 +1,8 @@
+void main() {
+    IO.print("Enter you want box :");
+    int box = Integer.parseInt(IO.readln()) ;
+    IO.print("Enter you you want pencil :");
+    int pencil = Integer.parseInt(IO.readln()) ;
+    int anwser = box * pencil ;
+    IO.println(" box is " + box + " Pencil is " + pencil + " anwser = " +  anwser );
+} 
