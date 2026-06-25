c3393e04c44174b8d52b222f5b1035ad1c1dbf69
kittisakdiw87 <kittisakdiw87@gmail.com>
2026-06-25 14:46:09 +0700

    first commit

---
+++ week2/sum.java
@@ -0,0 +1,7 @@
+// วิธีนี้ไม่ต้องง้อคลาส IO ในการรับค่า
+void main() {
+    int num1 = 4;
+    int num2 = 6;
+    sum = num1 + num2;
+    IO.println("Sum of " + num1 + " and " + num2 + " = " + sum);
+}
