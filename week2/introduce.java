713a488b257d5a24d7601ac2cb409de64cea133d
kittisakdiw87 <kittisakdiw87@gmail.com>
2026-06-25 16:24:02 +0700

    move introduce.java

---
+++ week2/introduce.java
@@ -0,0 +1,7 @@
+void main() {
+    IO.print("Enter your Name :");
+    String name = IO.readln() ;
+    IO.print("Enter your Age :");
+    int age = Integer.parseInt(IO.readln()) ;
+    IO.println("Hello " + name + " Age is " + age );
+}
